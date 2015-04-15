f7e09bb80e59274c098251a841cb073b10df9c70
Xawirses <michael.gileta@gmail.com>
2015-04-15 16:20:03 +0200

    Add flare.json generator

--- docker-viz.go
+++ docker-viz.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"github.com/Treeptik/docker-viz/flare"
 )
 
 func main() {
@@ -10,11 +11,11 @@ func main() {
 	r.LoadHTMLGlob("templates/*")
 	r.GET("/", func(c *gin.Context) {
 		obj := gin.H{"title": "Main website"}
-		c.HTML(http.StatusOK, "index_dendrogam.tmpl", obj)
+		c.HTML(http.StatusOK, "dendrogam.tmpl", obj)
 	})
 
 	r.GET("/flare.json", func(c *gin.Context) {
-		c.String(http.StatusOK, "json")
+		c.String(http.StatusOK, flare.Default())
 	})
 
 	// Listen and server on 0.0.0.0:8080
---
+++ flare/flareGenerator.go
@@ -0,0 +1,65 @@
+package flare
+
+import (
+	"github.com/samalba/dockerclient"
+	"log"
+)
+
+type DockerImage struct {
+	Id string
+	ParentId string
+	Name string
+	Create int64
+	Size int64
+	VirtualSize int64
+}
+
+func generateDockerImageList() map[string]DockerImage {
+	docker, _ := dockerclient.NewDockerClient("unix:///var/run/docker.sock", nil)
+
+	containers, err := docker.ListImages()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	images := make(map[string]DockerImage)
+	for _, c := range containers {
+		images[c.Id] = DockerImage{c.Id, c.ParentId, c.RepoTags[0], c.Created, c.Size, c.VirtualSize}
+	}
+
+	return images
+}
+
+func makeJson(imagesFilsList []string, dockerImagesFils map[string][]string, dockerImagesList map[string]DockerImage) string {
+	var flare string
+	nbFils := len(imagesFilsList)
+	var i int = 0
+	for _, image := range imagesFilsList {
+		i++
+		if _, ok := dockerImagesFils[image]; ok {
+			flare = flare + "{\"name\": \"" + dockerImagesList[image].Name + "\", \"children\": ["
+			flare = flare + makeJson(dockerImagesFils[image], dockerImagesFils, dockerImagesList) + "]}"
+		} else {
+			flare = flare + "{\"name\": \"" + dockerImagesList[image].Name + "\"}"
+		}
+		if i < nbFils {
+			flare = flare + ", "
+		}
+	}
+
+	return flare
+}
+
+func Default() string {
+	dockerImagesList := generateDockerImageList()
+	dockerImagesFils := make(map[string][]string)
+
+	for _, image := range dockerImagesList {
+		if _, ok := dockerImagesList[image.ParentId]; ok {
+			dockerImagesFils[image.ParentId] = append(dockerImagesFils[image.ParentId], image.Id)
+		} else {
+			dockerImagesFils["Docker"] = append(dockerImagesFils["Docker"], image.Id)
+		}
+	}
+	return  makeJson(dockerImagesFils["Docker"], dockerImagesFils, dockerImagesList)
+}
