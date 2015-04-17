d1ed1e423791d107df1f4fd5238770b18b76aff8
Xawirses <michael.gileta@gmail.com>
2015-04-17 16:36:11 +0200

    Add init for unit test

--- flare/flareGenerator_test.go
+++ flare/flareGenerator_test.go
@@ -1,16 +1,43 @@
-package viz
+package flare
 
 import (
-	"testing"
+	"github.com/samalba/dockerclient"
 	"github.com/stretchr/testify/assert"
+	"testing"
+	"os/exec"
+	"os"
+	"log"
 )
 
 func init() {
-	// Delete all containers
-	// Pull busybox
-	// Generate many containers and commit new images for a tree
+
+	_, err := exec.Command("./dockerInit.sh").Output()
+	if err != nil {
+		log.Fatalf("Init: %s", err)
+	}
+
 }
 
-func TestCreateEngine(t *testing.T) {
-	assert.Equal(t, 2, 2)
+func TestInitImagesAndContainers(t *testing.T) {
+	dockerClient := os.Getenv("DOCKER_HOST")
+	if dockerClient == "" {
+		dockerClient = "unix:///var/run/docker.sock"
+	}
+
+	docker, err := dockerclient.NewDockerClient(dockerClient, nil)
+	if err != nil {
+		t.Fatal("Cannot init the docker client")
+	}
+
+	images, err := docker.ListImages()
+	if err != nil {
+		log.Fatal(err)
+	}
+	containers, err := docker.ListContainers(true, true, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	assert.Equal(t, len(images), 48)
+	assert.Equal(t, len(containers), 13)
 }
