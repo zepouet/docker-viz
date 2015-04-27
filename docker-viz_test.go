8816b171935934c434e812f7c414775a3d92297a
Xawirses <michael.gileta@gmail.com>
2015-04-27 12:36:24 +0200

    unit test reborn

--- docker-viz_test.go
+++ docker-viz_test.go
@@ -12,19 +12,13 @@ func init() {
 }
 
 func TestLoadConfig(t *testing.T) {
-	vizPort, dockerClient := LoadConfig()
+	vizPort := LoadConfig()
 
 	os_vizPort := os.Getenv("VIZ_PORT")
-	os_dockerClient := os.Getenv("DOCKER_HOST")
 
 	if os_vizPort == "" {
 		os_vizPort = "8080"
 	}
 
-	if os_dockerClient == "" {
-		os_dockerClient = "unix:///var/run/docker.sock"
-	}
-
-	assert.Equal(t, os_dockerClient, dockerClient)
 	assert.Equal(t, os_vizPort, strconv.Itoa(vizPort))
 }
