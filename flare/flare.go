19c1508e69c88b61ccac347bca58292e61445c4c
Xawirses <michael.gileta@gmail.com>
2015-05-05 14:01:14 +0200

    Solved JsonFather bug (to many ,)

--- flare/flare.go
+++ flare/flare.go
@@ -1,6 +1,9 @@
 package flare
 
-import 	"github.com/Treeptik/docker-viz/dockertype"
+import (
+	"github.com/Treeptik/docker-viz/dockertype"
+	"fmt"
+)
 
 const(
 	BeginJson = "{\"name\": \"Docker\", \"children\": ["
@@ -10,19 +13,24 @@ const(
 // Create the json architecture with father/son table
 func MakeJsonFather(name string, dockerFils map[string][]string, dockerList map[string]dockertype.DockerType) string {
 	var flare string
-	nbFils := len(dockerFils[name])
 	var i int = 0
 	for _, fils := range dockerFils[name] {
-		i++
 		if _, ok := dockerFils[fils]; ok {
+			if i != 0 {
+				flare += ", "
+			}
 			flare += "{\"name\": \"" + dockerList[fils].GetName() + "\", \"children\": ["
 			flare += MakeJsonFather(fils, dockerFils, dockerList) + "]}"
 		} else {
+			if dockerList[fils].GetSize() == "0" {
+				continue
+			}
+			if i != 0 {
+				flare += ", "
+			}
 			flare += "{\"name\": \"" + dockerList[fils].GetName() + "\", \"size\": " + dockerList[fils].GetSize() + "}"
 		}
-		if i < nbFils {
-			flare += ", "
-		}
+		i++
 	}
 
 	return flare
