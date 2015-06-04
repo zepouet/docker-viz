0b7cd5bce7288e11f726936ac50bc59459c5666b
Nicolas <zepouet@gmail.com>
2015-06-04 16:54:22 +0200

    Add timer for testing api call

--- flare/flareBubble.go
+++ flare/flareBubble.go
@@ -2,12 +2,15 @@ package flare
 
 import (
 	"github.com/Treeptik/docker-viz/dockertype"
+	"log"
+	"time"
 )
 
 // return all json for Bubble diagram
 func BubbleFlare(who string) string {
 	dockerList := make(map[string]dockertype.DockerType)
 
+	start := time.Now()
 	switch who {
 		case "images":
 			dockerList = dockertype.GenerateDockerImageList()
@@ -15,8 +18,13 @@ func BubbleFlare(who string) string {
 			dockerList = dockertype.GenerateDockerContainerList()
 		default:
 	}
+	elapsed := time.Since(start)
+	log.Printf("GenerateDockerContainerList took %s", elapsed)
 
+	start = time.Now()
 	dockerImagesChilds := dockertype.GenerateDockerChild(dockerList)
+	elapsed = time.Since(start)
+	log.Printf("GenerateDockerChild took %s", elapsed)
 
 	return  BeginJson + MakeJsonFather("Docker", dockerImagesChilds, dockerList) + EndJson
 }
